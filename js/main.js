170328d76e8e75db38041a3b4045c9e935ebb9ef
Tanya <ta.zharska@gmail.com>
2017-02-27 18:11:28 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*jQuery(document).ready(function( $ ) {
+      $("#menu").mmenu();
+   });*/
+
+$(document).ready(function() {
+      $("#my-menu").mmenu({
+      });
+
+
+      $(".owl-carousel").owlCarousel({
+        loop: true,
+        items: 1,
+        autoplay:true,
+        autoplayTimeout:2500,
+        autoplayHoverPause:true
+
+
+
+      });
+    
+});
+
+
+
+
